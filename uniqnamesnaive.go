2cd27204ce0898c1380b3d5ba0d0af6eac36ccc8
Roman Dibikhin <dibikhin@gmail.com>
2021-01-29 12:51:05 +0300

    renamed

---
+++ uniqnamesnaive.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func uniqueNames(a, b []string) []string {
+	m := make(map[string]struct{})
+
+	for i := range a {
+		m[a[i]] = struct{}{}
+	}
+	for i := range b {
+		m[b[i]] = struct{}{}
+	}
+	var result []string
+	for k := range m {
+		result = append(result, k)
+	}
+	return result
+}
+
+func main() {
+	// should print Ava, Emma, Olivia, Sophia
+	fmt.Println(uniqueNames(
+		[]string{"Ava", "Emma", "Olivia"},
+		[]string{"Olivia", "Sophia", "Emma"}))
+	fmt.Println(uniqueNames(
+		[]string{"Ava", "Emma", "Olivia"},
+		[]string{"Sophia"}))
+}
